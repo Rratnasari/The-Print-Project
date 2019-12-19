018bc457838b382f668b1b52aac5a50ef30c8736
MOKA <moka@MOKAs-MacBook-Pro.local>
2019-12-19 14:28:13 +0700

    initial commit

---
+++ src/TestPrint.java
@@ -0,0 +1,10 @@
+public class TestPrint {
+
+  public static void main(String[] args) {
+    System.out.println("Hello New World!");
+    //initialize git > terminal > pwd > git init > ls -al (shift+command+.)
+    //gitignore file > file yang gak mau dimasukin ke repository (vi .gitignore > click i > esc > :wq)
+    //cek (git status)
+
+  }
+}
